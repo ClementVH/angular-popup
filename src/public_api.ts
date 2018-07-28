201eaa4804b56951638a82caba356aaf13f663d3
ClementVH <clement.vanhecke@gmail.com>
2018-07-29 01:07:53 +0200

    export popup service

--- src/public_api.ts
+++ src/public_api.ts
@@ -1,4 +1,5 @@
 export * from "./popup.module";
+export * from "./popup.service";
 export * from "./items/confirm-popup-item";
 export * from "./items/html-popup-item";
 export * from "./items/iframe-popup-item";
